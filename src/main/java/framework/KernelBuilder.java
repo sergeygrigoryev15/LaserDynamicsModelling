893d2bdf315085dfcb09a5205dbf162f9b8c32dd
sergeygrigoryev15 <metalloman93@gmail.com>
2017-06-03 13:47:00 +0300

    Saturable absorber is evaluated now.

--- src/main/java/framework/KernelBuilder.java
+++ src/main/java/framework/KernelBuilder.java
@@ -5,18 +5,16 @@ import framework.entities.resonatorParts.ActiveMedia;
 import framework.entities.resonatorParts.BaseResonatorPart;
 import framework.entities.resonatorParts.SaturableAbsorber;
 import framework.enums.ResonatorElementType;
+import kernels.ClStringConstants;
 
-import java.util.Arrays;
 import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.Collectors;
 
 import static framework.Constants.*;
 
 public class KernelBuilder {
 
     private static Resonator resonator;
-
-    private static String mainKernelPartTemplate =
-            "__kernel void %1$s(__global float* inPLUS, __global float* outPLUS,__global float* inMINUS,__global float* outMINUS){%n%2$s}";
 
     private static StringBuilder programSource = new StringBuilder();
 
@@ -35,19 +33,23 @@ public class KernelBuilder {
         setResonator(resonator);
     }
 
-    private int[] getResonatorPartsIndexes() {
-        int[] points = new int[getResonator().getParts().size() - 1];
+    private long[] getResonatorPartsIndexes() {
+        long[] points = new long[getResonator().getParts().size() - 1];
         final AtomicInteger counter = new AtomicInteger(1);
-        return Arrays.stream(points).map(point -> {
-            double sumLength = getResonator().getParts().values().stream().limit(counter.getAndIncrement()).mapToDouble(BaseResonatorPart::getLength).sum();
-            return (int) (getResonator().getLength() * sumLength / Constants.resonatorPointsNumber - 1);
-        }).toArray();
+        for (int i = 0; i < points.length; i++) {
+            double sum = 0;
+            for (BaseResonatorPart part : getResonator().getParts().values().stream().limit(counter.getAndIncrement()).collect(Collectors.toList())) {
+                sum += part.getLength();
+            }
+            points[i] = Math.round((Constants.resonatorPointsNumber / getResonator().getLength()) * sum - 1);
+        }
+        return points;
     }
 
     public String buildKernel() {
         programSource.append(defineConstants());
         programSource.append(defineFunctions());
-        programSource.append(defineMainPart());
+        programSource.append(defineMainPart(getResonatorPartsIndexes()));
         return programSource.toString();
     }
 
@@ -58,8 +60,8 @@ public class KernelBuilder {
         saturableAbsorber = ((SaturableAbsorber) getResonator().getParts().get(ResonatorElementType.SATURABLE_ABSORBER));
 
         //We assume that dx/dt = c/2;
-        double time = Constants.time*getResonator().getLength()/speedOfLight;
-        long numberTpoints = Math.round((speedOfLight/2)*(time/getResonator().getLength())*resonatorPointsNumber);
+        double time = Constants.time * getResonator().getLength() / speedOfLight;
+        long numberTpoints = Math.round((speedOfLight / 2) * (time / getResonator().getLength()) * resonatorPointsNumber);
 
         builder.append(String.format("__constant float maxtime = %s;\n", String.valueOf(time)));
         builder.append(String.format("__constant int numberXpoints = %s;\n", String.valueOf(resonatorPointsNumber)));
@@ -90,105 +92,157 @@ public class KernelBuilder {
     private String defineFunctions() {
         StringBuilder builder = new StringBuilder();
 
-        double functionI = (pumpPower*pumpWaveLength)/(pumpCrossSection*speedOfLight*planksconst);
-        double functionAlpha = ((saturableAbsorber.getAbsorption()+saturableAbsorber.getEmission())/(activeMedia.getEmission()*activeMedia.getLifeTime()))
-                *saturableAbsorber.getLifeTime()*(1+ activeMedia.getAbsorption()*activeMedia.getLifeTime()*functionI);
-        double koefficientA = (activeMedia.getLength()*activeMedia.getIonsConcentration()*functionI*activeMedia.getAbsorption()*activeMedia.getEmission()*activeMedia.getLifeTime())
-                /(internalLoses*(1+ activeMedia.getAbsorption()*activeMedia.getLifeTime()*functionI));
-        double tempKoeff = internalLoses/(refractionIndex*(activeMedia.getLength()+saturableAbsorber.getLength()));
-        double rKoeff = (-1)*tempKoeff*speedOfLight;
-        double dKoeff = ((1/activeMedia.getLifeTime())+(activeMedia.getAbsorption()*((pumpPower*pumpWaveLength)/(pumpCrossSection*planksconst*speedOfLight))));
+        double functionI = (pumpPower * pumpWaveLength) / (pumpCrossSection * speedOfLight * planksconst);
+        double functionAlpha = ((saturableAbsorber.getAbsorption() + saturableAbsorber.getEmission()) / (activeMedia.getEmission() * activeMedia.getLifeTime()))
+                * saturableAbsorber.getLifeTime() * (1 + activeMedia.getAbsorption() * activeMedia.getLifeTime() * functionI);
+        double koefficientA = (activeMedia.getLength() * activeMedia.getIonsConcentration() * functionI * activeMedia.getAbsorption() * activeMedia.getEmission() * activeMedia.getLifeTime())
+                / (internalLoses * (1 + activeMedia.getAbsorption() * activeMedia.getLifeTime() * functionI));
 
-        String str =
-                        "float I(){return %1$s;}\n" +
-                        "float alpha(){return %2$s;}\n" +
-                        "float koefficient_A(){return %3$s;}\n" +
-                        "float aPLUS(float x, float u){return %4$s*((koefficient_A()*x - 1)+ (1-x)*(exp(-u)*R_lum));}\n" +
-                        "float aMINUS(float x, float u){return %4$s*((koefficient_A()*x - 1)+ (1-x)*(exp(-u)*R_lum));}\n" +
-                        "float rPLUS(float x){return x*%5$s;}\n" +
-                        "float rMINUS(float x){return x*%5$s;}\n" +
-                        "float d_AM_func(float d_AM, float uPLUS, float uMINUS, float x){" +
-                                "return ((1/AM_lifeTime)+(absorption*I()*exp((-1)*absorption*AM_ionsConc*x)))*(1-(1+(exp(uPLUS) + exp(uMINUS)))*d_AM);}\n";
-        builder.append(String.format(str, String.valueOf(functionI), String.valueOf(functionAlpha), String.valueOf(koefficientA),
-                String.valueOf(tempKoeff), String.valueOf(rKoeff), String.valueOf(dKoeff)));
+        double absorberA = (saturableAbsorber.getLength() * saturableAbsorber.getAbsorption() * saturableAbsorber.getIonsConcentration() * speedOfLight)
+                / (refractionIndex * (saturableAbsorber.getLength() + activeMedia.getLength()));
+        double rShort = speedOfLight / (refractionIndex * (activeMedia.getLength() + saturableAbsorber.getLength()));
+        double r = rShort * internalLoses;
+
+        builder.append(String.format(ClStringConstants.functions, String.valueOf(functionI), String.valueOf(functionAlpha), String.valueOf(koefficientA),
+                String.valueOf(r), String.valueOf(absorberA), String.valueOf(rShort)));
         return builder.toString();
     }
 
-    private String defineMainPart() {
+    private String getStepTemplate(final long[] points, final String strForAM, final String strForSA, final String strForV) {
+        StringBuilder builder = new StringBuilder();
+        ResonatorElementType[] resonatorParts = getResonator().getParts().keySet().toArray(new ResonatorElementType[0]);
+        String str = "if(i>=%1$s && i<%2$s){%n %3$s}%n";
+        String tmp = "";
+        if (points.length == 0) {
+            switch (resonatorParts[0]) {
+                case ACTIVE_MEDIA:
+                    builder.append(strForAM);
+                    break;
+                case SATURABLE_ABSORBER:
+                    builder.append(strForSA);
+                    break;
+                case VACUUM:
+                    builder.append(strForV);
+                    break;
+            }
+        } else {
+            for (int i = 0; i < points.length; i++) {
+                switch (resonatorParts[i]) {
+                    case ACTIVE_MEDIA:
+                        tmp = strForAM;
+                        break;
+                    case SATURABLE_ABSORBER:
+                        tmp = strForSA;
+                        break;
+                    case VACUUM:
+                        tmp = strForV;
+                        break;
+                }
+                if (i == 0) {
+                    builder.append(String.format(str, 0, points[i], tmp));
+                } else if (i == points.length - 1) {
+                    builder.append(String.format(str, points[i], Constants.resonatorPointsNumber, tmp));
+                } else {
+                    builder.append(String.format(str, points[i - 1], points[i - 1], tmp));
+                }
+            }
+        }
+        switch (resonatorParts[points.length]) {
+            case ACTIVE_MEDIA:
+                tmp = strForAM;
+                break;
+            case SATURABLE_ABSORBER:
+                tmp = strForSA;
+                break;
+            case VACUUM:
+                tmp = strForV;
+        }
+        builder.append(String.format(str, points[points.length - 1], Constants.resonatorPointsNumber, tmp));
+        return builder.toString();
+    }
+
+    private String getMiddleStep(final long[] points) {
+        return getStepTemplate(points, ClStringConstants.activeMediaMiddleStep, ClStringConstants.saturableAbsorberMiddleStep, "");
+    }
+
+    private String getMainStep(final long[] points) {
+        return getStepTemplate(points, ClStringConstants.activeMediaStep, ClStringConstants.saturableAbsorberStep, ClStringConstants.vacuumStep);
+    }
+
+    private String getInversionStep(final long[] points) {
+        return getStepTemplate(points, ClStringConstants.activeMediaInversionStep, ClStringConstants.saturableAbsorberInversionStep, "");
+    }
+
+    private String defineMainPart(final long[] points) {
         StringBuilder builder = new StringBuilder();
         String str =
-                        "__private float leng = AM_L + SA_L;\n" +
+                "__private float leng = AM_L + SA_L;\n" +
                         "__private float dx = leng/(numberXpoints-1);\n" +
-                        "__private int numberAMpoints = (int)numberXpoints*AM_L/leng;\n" +
-                        "//__private int numberSApoints = (int)numberXpoints*SA_L/leng;\n" +
-                        "__private int numberSApoints = numberXpoints - numberAMpoints;\n" +
+                        "__private float dt = maxtime/numberTpoints;\n" +
                         "__private int nk = get_global_size(0);\n" +
                         "__private int startX;\n" +
                         "__private int endX;\n" +
                         "const int numberOfX = numberXpoints;\n" +
-                        "__private float dt = maxtime/numberTpoints;\n" +
                         "__private float middleStepPLUS[%2$d];\n" +
                         "__private float middleStepMINUS[%2$d];\n" +
                         "__private float tmpPlus;\n" +
                         "__private float tmpMinus;\n" +
                         "__local float plus[%1$d];\n" +
                         "__local float minus[%1$d];\n" +
-                        "__local float D_AM[%1$d];\n" +
-                        "float k1,k2,k3,k4,k5,k6,k7,k8,tmpD_AM;\n" +
+                        "__local float D[%1$d];\n" +
+                        "float k1,k2,k3,k4,k5,k6,k7,k8,tmpD;\n" +
                         "int num = get_global_id(0);\n" +
+
                         "startX = num*(numberOfX)/nk;\n" +
                         "if(num != nk-1){\n" +
                         "endX = (num+1)*(numberOfX)/nk;\n" +
                         "} else {\n" +
                         "endX = numberOfX;\n" +
                         "}\n" +
+
                         "for(int i = startX; i < endX; i++){\n" +
                         "plus[i] = inPLUS[i];\n" +
                         "minus[i] = inMINUS[i];\n" +
-                        "D_AM[i] = 0.1;\n" +
+                        "D[i] = 0.1;\n" +
                         "}\n" +
+
                         "barrier(CLK_LOCAL_MEM_FENCE);\n" +
+
                         "for(int t = 0;t <= numberTpoints; t++){\n" +
+
                         "for(int i = startX; i <= endX; i++){\n" +
-                        "if(i == 0){\n" +
-                        "middleStepPLUS[i] = plus[i]+(aPLUS(D_AM[i],plus[i]) + aPLUS(D_AM[i],plus[i]))*(dt/4);\n" +
-                        "middleStepMINUS[i] = minus[i]+(aMINUS(D_AM[i],minus[i]) + aMINUS(D_AM[i],minus[i]))*(dt/4);\n" +
-                        "}else if(i == numberOfX) {\n" +
-                        "middleStepPLUS[i] = plus[i-1] + (aPLUS(D_AM[i-1],plus[i-1]) + aPLUS(D_AM[i-1],plus[i-1]))*(dt/4);\n" +
-                        "middleStepMINUS[i] = minus[i-1] + (aMINUS(D_AM[i-1],minus[i-1]) + aMINUS(D_AM[i-1],minus[i-1]))*(dt/4);\n" +
-                        "} else{\n" +
-                        "middleStepPLUS[i] = (plus[i-1] + plus[i])/2 + (rPLUS(plus[i])-rPLUS(plus[i-1]))/(2) + (aPLUS(D_AM[i-1],plus[i-1]) + aPLUS(D_AM[i],plus[i]))*(dt/4);\n" +
-                        "middleStepMINUS[i] = (minus[i-1] + minus[i])/2 + (rMINUS(minus[i])-rMINUS(minus[i-1]))/(2) + (aMINUS(D_AM[i-1],minus[i-1]) + aMINUS(D_AM[i],minus[i]))*(dt/4);\n" +
+
+                        getMiddleStep(points) +
+
                         "}\n" +
-                        "}\n" +
+
                         "barrier(CLK_LOCAL_MEM_FENCE);\n" +
                         "for(int i = startX; i < endX; i++){\n" +
-                        "tmpPlus = plus[i] + (rPLUS(middleStepPLUS[i+1]) - rPLUS(middleStepPLUS[i]))/c + (aPLUS(D_AM[i],plus[i]) + aPLUS(D_AM[i],plus[i]))*(dt/2);\n" +
-                        "tmpMinus = minus[i] + (rMINUS(middleStepMINUS[i+1]) - rMINUS(middleStepMINUS[i]))/c + (aMINUS(D_AM[i],minus[i]) + aMINUS(D_AM[i],minus[i]))*(dt/2);\n" +
+
+                        getMainStep(points) +
+
                         "barrier(CLK_LOCAL_MEM_FENCE);\n" +
                         "plus[i] = tmpPlus;\n" +
                         "minus[i] = tmpMinus;\n" +
-                        "k1= d_AM_func(D_AM[i],plus[i],minus[i],i*dx)*dt;\n" +
-                        "k2= d_AM_func(D_AM[i]+k1/5.,plus[i],minus[i],i*dx)*dt;\n" +
-                        "k3= d_AM_func(D_AM[i]+k1*3/40.+k2*9/40.,plus[i],minus[i],i*dx)*dt;\n" +
-                        "k4= d_AM_func(D_AM[i]+k1*44/45.-k2*56/15.+k3*32/9.,plus[i],minus[i],i*dx)*dt;\n" +
-                        "k5= d_AM_func(D_AM[i]+k1*19372/6561.-k2*25360/2187.+k3*64448/6561.-k4*212/729.,plus[i],minus[i],i*dx)*dt;\n" +
-                        "k6= d_AM_func(D_AM[i]+k1*9017/3168.-k2*355/33.+k3*46732/5247.+k4*49/176.-k5*5103/18656,plus[i],minus[i],i*dx)*dt;\n" +
-                        "k7= d_AM_func(D_AM[i]+k1*35/384.+k3*500/1113.+k4*125/192.-k5*2187/6784.+k6*11/84.,plus[i],minus[i],i*dx)*dt;\n" +
+
+                        getInversionStep(points) +
                         "k8= k1*35/384.+k3*500/1113.+k4*125/192.-k5*2187/6784.+k6*11/84.;\n" +
-                        "tmpD_AM = D_AM[i] + k8;\n" +
-                        "D_AM[i] = tmpD_AM;\n" +
+                        "tmpD = D[i] + k8;\n" +
+                        "D[i] = tmpD;\n" +
+
                         "}\n" +
+
                         "barrier(CLK_LOCAL_MEM_FENCE);\n" +
                         "plus[0] = minus[numberOfX-1] + log(r1);\n" +
                         "minus[0] = plus[numberOfX-1] + log(r2);\n" +
                         "}\n" +
-                        "/*for(int i = startX; i < endX; i++){\n" +
+
+                        "for(int i = startX; i < endX; i++){\n" +
                         "outPLUS[i] = plus[i];\n" +
-                        "outMINUS[i] = D_AM[i];\n" +
+                        "outMINUS[i] = minus[i];\n" +
                         "}\n" +
-                        "barrier(CLK_GLOBAL_MEM_FENCE);*/\n";
-        builder.append(String.format(mainKernelPartTemplate, kernelName, String.format(str, resonatorPointsNumber, resonatorPointsNumber + 1)));
+                        "barrier(CLK_GLOBAL_MEM_FENCE);\n";
+        builder.append(String.format(ClStringConstants.mainKernelPartTemplate, kernelName, String.format(str, resonatorPointsNumber, resonatorPointsNumber + 1)));
         return builder.toString();
     }
 }
---
+++ src/main/java/kernels/ClStringConstants.java
@@ -0,0 +1,67 @@
+package kernels;
+
+public class ClStringConstants {
+    public static String mainKernelPartTemplate =
+            "__kernel void %1$s(__global float* inPLUS, __global float* outPLUS,__global float* inMINUS,__global float* outMINUS){%n%2$s}";
+    public static String functions =
+            "float I(){return %1$s;}\n" +
+                    "float alpha(){return %2$s;}\n" +
+                    "float koefficient_A(){return %3$s;}\n" +
+                    "float a_AM_PLUS(float x, float u){return %4$s*((koefficient_A()*x - 1)+ (1-x)*(exp(-u)*R_lum));}\n" +
+                    "float a_AM_MINUS(float x, float u){return a_AM_PLUS(x, u);}\n" +
+                    "float a_SA_PLUS(float x){return %5$s*x + %4$s;}\n" +
+                    "float a_SA_MINUS(float x){return a_SA_PLUS(x);}\n" +
+                    "float r_AM_PLUS(float x){return x*%4$s;}\n" +
+                    "float r_AM_MINUS(float x){return -r_AM_PLUS(x);}\n" +
+                    "float r_SA_PLUS(float x){return r_AM_PLUS(x);}\n" +
+                    "float r_SA_MINUS(float x){return r_AM_MINUS(x);}\n" +
+                    "float d_AM_func(float d_AM, float uPLUS, float uMINUS, float x){" +
+                    "return ((1/AM_lifeTime)+(absorption*I()*exp(-absorption*AM_ionsConc*x)))*(1-(1+(exp(uPLUS) + exp(uMINUS)))*d_AM);}\n" +
+                    "float d_SA_func(float d_SA, float uPLUS, float uMINUS){" +
+                    "return %6$s*(-1 - (1 + alpha()*(exp(uPLUS) + exp(uMINUS)))*d_SA);}\n";
+    public static final String activeMediaMiddleStep =
+            "if(i == 0){\n" +
+                    "middleStepPLUS[i] = plus[i]+(a_AM_PLUS(D[i],plus[i]) + a_AM_PLUS(D[i],plus[i]))*(dt/4);\n" +
+                    "middleStepMINUS[i] = minus[i]+(a_AM_MINUS(D[i],minus[i]) + a_AM_MINUS(D[i],minus[i]))*(dt/4);\n" +
+                    "}else if(i == numberOfX) {\n" +
+                    "middleStepPLUS[i] = plus[i-1] + (a_AM_PLUS(D[i-1],plus[i-1]) + a_AM_PLUS(D[i-1],plus[i-1]))*(dt/4);\n" +
+                    "middleStepMINUS[i] = minus[i-1] + (a_AM_MINUS(D[i-1],minus[i-1]) + a_AM_MINUS(D[i-1],minus[i-1]))*(dt/4);\n" +
+                    "} else{\n" +
+                    "middleStepPLUS[i] = (plus[i-1] + plus[i])/2 + (r_AM_PLUS(plus[i])-r_AM_PLUS(plus[i-1]))*(dt/(2*dx)) + (a_AM_PLUS(D[i-1],plus[i-1]) + a_AM_PLUS(D[i],plus[i]))*(dt/4);\n" +
+                    "middleStepMINUS[i] = (minus[i-1] + minus[i])/2 + (r_AM_MINUS(minus[i])-r_AM_MINUS(minus[i-1]))*(dt/(2*dx)) + (a_AM_MINUS(D[i-1],minus[i-1]) + a_AM_MINUS(D[i],minus[i]))*(dt/4);\n" +
+                    "}\n";
+    public static final String saturableAbsorberMiddleStep =
+            "if(i == 0){\n" +
+                    "middleStepPLUS[i] = plus[i]+(a_SA_PLUS(D[i]) + a_SA_PLUS(D[i]))*(dt/4);\n" +
+                    "middleStepMINUS[i] = minus[i]+(a_SA_MINUS(D[i]) + a_SA_MINUS(D[i]))*(dt/4);\n" +
+                    "}else if(i == numberOfX) {\n" +
+                    "middleStepPLUS[i] = plus[i-1] + (a_SA_PLUS(D[i-1]) + a_SA_PLUS(D[i-1]))*(dt/4);\n" +
+                    "middleStepMINUS[i] = minus[i-1] + (a_SA_MINUS(D[i-1]) + a_SA_MINUS(D[i-1]))*(dt/4);\n" +
+                    "} else{\n" +
+                    "middleStepPLUS[i] = (plus[i-1] + plus[i])/2 + (r_SA_PLUS(plus[i])-r_SA_PLUS(plus[i-1]))*(dt/(2*dx)) + (a_SA_PLUS(D[i-1]) + a_SA_PLUS(D[i]))*(dt/4);\n" +
+                    "middleStepMINUS[i] = (minus[i-1] + minus[i])/2 + (r_SA_MINUS(minus[i])-r_SA_MINUS(minus[i-1]))*(dt/(2*dx)) + (a_SA_MINUS(D[i-1]) + a_SA_MINUS(D[i]))*(dt/4);\n" +
+                    "}\n";
+    public static final String activeMediaStep =
+            "tmpPlus = plus[i] + (r_AM_PLUS(middleStepPLUS[i+1]) - r_AM_PLUS(middleStepPLUS[i]))*(dt/dx) + (a_AM_PLUS(D[i],plus[i]) + a_AM_PLUS(D[i],plus[i]))*(dt/2);\n" +
+                    "tmpMinus = minus[i] + (r_AM_MINUS(middleStepMINUS[i+1]) - r_AM_MINUS(middleStepMINUS[i]))*(dt/dx) + (a_AM_MINUS(D[i],minus[i]) + a_AM_MINUS(D[i],minus[i]))*(dt/2);\n";
+    public static final String saturableAbsorberStep =
+            "tmpPlus = plus[i] + (r_SA_PLUS(middleStepPLUS[i+1]) - r_SA_PLUS(middleStepPLUS[i]))*(dt/dx) + (a_SA_PLUS(D[i]) + a_SA_PLUS(D[i]))*(dt/2);\n" +
+                    "tmpMinus = minus[i] + (r_SA_MINUS(middleStepMINUS[i+1]) - r_SA_MINUS(middleStepMINUS[i]))*(dt/dx) + (a_SA_MINUS(D[i]) + a_SA_MINUS(D[i]))*(dt/2);\n";
+    public static final String vacuumStep = "";
+    public static final String activeMediaInversionStep =
+            "k1= d_AM_func(D[i],plus[i],minus[i],i*dx)*dt;\n" +
+                    "k2= d_AM_func(D[i]+k1/5.,plus[i],minus[i],i*dx)*dt;\n" +
+                    "k3= d_AM_func(D[i]+k1*3/40.+k2*9/40.,plus[i],minus[i],i*dx)*dt;\n" +
+                    "k4= d_AM_func(D[i]+k1*44/45.-k2*56/15.+k3*32/9.,plus[i],minus[i],i*dx)*dt;\n" +
+                    "k5= d_AM_func(D[i]+k1*19372/6561.-k2*25360/2187.+k3*64448/6561.-k4*212/729.,plus[i],minus[i],i*dx)*dt;\n" +
+                    "k6= d_AM_func(D[i]+k1*9017/3168.-k2*355/33.+k3*46732/5247.+k4*49/176.-k5*5103/18656,plus[i],minus[i],i*dx)*dt;\n" +
+                    "k7= d_AM_func(D[i]+k1*35/384.+k3*500/1113.+k4*125/192.-k5*2187/6784.+k6*11/84.,plus[i],minus[i],i*dx)*dt;\n";
+    public static final String saturableAbsorberInversionStep =
+            "k1= d_SA_func(D[i],plus[i],minus[i])*dt;\n" +
+                    "k2= d_SA_func(D[i]+k1/5.,plus[i],minus[i])*dt;\n" +
+                    "k3= d_SA_func(D[i]+k1*3/40.+k2*9/40.,plus[i],minus[i])*dt;\n" +
+                    "k4= d_SA_func(D[i]+k1*44/45.-k2*56/15.+k3*32/9.,plus[i],minus[i])*dt;\n" +
+                    "k5= d_SA_func(D[i]+k1*19372/6561.-k2*25360/2187.+k3*64448/6561.-k4*212/729.,plus[i],minus[i])*dt;\n" +
+                    "k6= d_SA_func(D[i]+k1*9017/3168.-k2*355/33.+k3*46732/5247.+k4*49/176.-k5*5103/18656,plus[i],minus[i])*dt;\n" +
+                    "k7= d_SA_func(D[i]+k1*35/384.+k3*500/1113.+k4*125/192.-k5*2187/6784.+k6*11/84.,plus[i],minus[i])*dt;\n";
+}
